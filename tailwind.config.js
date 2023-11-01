1b369b9ce65f07d1da1e9314017fff717a14bd54
Ferdiyansyah59 <ferdir5599@gmail.com>
2023-11-01 20:28:47 +0700

    color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: '#8B9E70',
+        secondary: '#B5C1A3',
+        footer: '#8B9E70',
+        plastic: '#9BC188',
+        battery: '#323516',
+      },
+      colors: {
+        primary: '#323516',
+      },
+    },
   },
   plugins: [],
 };
